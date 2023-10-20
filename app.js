840911c9ae57ade52b702fc2113baeca4f688710
Connor Gunter <connorgunter@Connors-MacBook-Pro.local>
2023-10-20 16:34:34 -0400

    complete task 1

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
 // Task 1 - Notes for JS Evaluation Predictions
+//4 + 4 / 2 -> 6
+// (2 + -7) * 3 -> 15
+// 101 % 10 -> 1
+// 12 - 4 % 3 -> 11
+// true && false -> false
+// true && !(false || false) -> true
+// !true && !(false || false) - > false
+// 'cat' + 'dog' -> 'catdog'
+// 2 + 'pizza' -> 2pizza
+// 2.5 * 'fish' -> NaN
+// 5 >= 11 -> false
+// 5 === 5.0 -> true
+// 7 !== 7.1 -> true
+// 5 + 5 > 8 -> true
+// 6 + 6 !== 12 -> false
+// 2 > 1 || false
+// 'true' === true
+// 10 % 2 === 0
+// 21 % 2 === 0
+// 21 % 2 !== 0
+// 21 % 2 === 1
+// 12 % 3 === 0
+// 9 % 3 === 0
+// 14 % 3 === 0
+// 'new york'[0]
+// 'new york'[1]
+// 'san francisco'[2 * 3]
+// 'engineering'[3].toUpperCase()
+// 'engineering'.indexOf('G')
+// 'engineering'.indexOf('neer')
+// 'engineering'.indexOf('r') > -1
+// 'science'.indexOf('x') === -1
 
 // List 3 expressions that surprised you or gave unexpected results, why?
-
+//2 + 'pizza' surpised me because I figured it would have been undefined, my thought process was you cant add a string a number together
+// 6 + 6 !== 12 this one surprised me 
 // List 2 exprsssions you felt comfortable interpreting at a glance.
 
 // List 1 expression you are still 'shaky' on, why?
