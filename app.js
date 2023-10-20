b140492baccb3168b65e76d908c4246630ac19e8
Jeff Mancilla <jeffmancilla@outlook.com>
2023-10-20 14:33:41 -0500

    complete task1, start task2

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 // Task 1 - Notes for JS Evaluation Predictions
 
 // List 3 expressions that surprised you or gave unexpected results, why?
+// - math operations beyond adding numbers to strings result in a NaN output? 
+// - numbers that equate to the same result is considered true (not truthy) in JS, even if there is a different between "subtypes(?)" (float vs integer) 
+// - if the argument .indexOf() does not exist within the string/array, it will return -1
 
 // List 2 exprsssions you felt comfortable interpreting at a glance.
+// - order of operations *does* apply with equations
 
 // List 1 expression you are still 'shaky' on, why?
+// - order of operations *does* apply with equations - i frequently forget my order of operations, lol
 
 
 
 
 
 // Task 2 - Practice JS Basics
-// 1 - 
+// 1 - 10, 10
+let idx = 'abcde'.indexOf('D')
+idx = idx + 11
+console.log(idx)
+idx * 2
+console.log(idx)
 
 // 2 - 
 
