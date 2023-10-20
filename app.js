725bc7debb9194ee22bd1adc1fe9168a739313be
Parth Vachhani <vachhani.parth.96@gmail.com>
2023-10-20 14:48:46 -0400

    entered predictions for Task 1 in notes section

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 // Task 1 - Notes for JS Evaluation Predictions
-
+// 1. Prediction: 6
+// 2. Prediction: -15
+// 3. Prediction: 1
+// 4. Prediction: 11
+// 5. Prediction: false
+// 6. Prediction: true
+// 7. Prediction: false
+// 8. Prediction: catdog
+// 9. Prediction: 2pizza
+// 10. Prediction: undefined
+// 11. Prediction: false
+// 12. Prediction: false
+// 13. Prediction: true
+// 14. Prediction: true
+// 15. Prediction: false
+// 16. Prediction: true
+// 17. Prediction: true
+// 18. Prediction: true
+// 19. Prediction: false
+// 20. Prediction: true
+// 21. Prediction: true
+// 22. Prediction: true
+// 23. Prediction: true
+// 24. Prediction: false
+// 25. Prediction: n
+// 26. Prediction: e
+// 27. Prediction: a
+// 28. Prediction: I
+// 29. Prediction: undefined
+// 30. Prediction: undefined
+// 31. Prediction: true
+// 32. Prediction: false
 // List 3 expressions that surprised you or gave unexpected results, why?
 
 // List 2 exprsssions you felt comfortable interpreting at a glance.
