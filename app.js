1dfa4257ab211790666960c6603109dc8ff9c386
roguione <myjobcontacts@yahoo.com>
2023-10-20 14:25:40 -0500

    notes1

--- app.js
+++ app.js
@@ -1,37 +1,81 @@
-/* Task 1 - Notes for JS Evaluation Predictions
+Task 1 - Notes for JS Evaluation Predictions
 
-    N1.  JavaScript Expressions: JavaScript expressions are evaluated from left to right 
-         based on operator precedence. 
+N1.  JavaScript Expressions: JavaScript expressions are evaluated from left to right 
+     based on operator precedence. 
 
-    N2.  Operator Precedence: JavaScript follows the order of operations (PEMDAS/BODMAS): 
-         Parentheses, Exponents, Multiplication/Division (left to right), 
-         Addition/Subtraction (left to right). 
-            1.   Parentheses
-            2.   Exponents
-            3.   Multiplication and Division (left to right)
-            4.   Addition and Subtraction (left to right) 
+N2.  Operator Precedence: JavaScript follows the order of operations (PEMDAS/BODMAS): 
+     Parentheses, Exponents, Multiplication/Division (left to right), 
+     Addition/Subtraction (left to right). 
+        1.   Parentheses
+        2.   Exponents
+        3.   Multiplication and Division (left to right)
+        4.   Addition and Subtraction (left to right) 
 
-    N3.  Evaluation Order: Understanding the order in which operators are applied 
-         is crucial for predicting the result of complex expressions
+N3.  Evaluation Order: Understanding the order in which operators are applied 
+     is crucial for predicting the result of complex expressions
 
-    N4.  Use Parentheses: To explicitly control the order of evaluation, use parentheses. 
-                "For example, (2 + 2) / 2 would result in 2"
+N4.  Use Parentheses: To explicitly control the order of evaluation, use parentheses. 
+            "For example, (2 + 2) / 2 would result in 2"
    
-    N5.  Testing and Debugging: It's a good practice to test and debug your JavaScript code 
-         to ensure the expected results.
+N5.  Testing and Debugging: It's a good practice to test and debug your JavaScript code 
+     to ensure the expected results.
 
-    N6.  Documentation: Refer to JavaScript documentation or resources for detailed information 
-         on operator precedence and expressions. 
+N6.  Documentation: Refer to JavaScript documentation or resources for detailed information 
+     on operator precedence and expressions. 
 
 
 //List 3 expressions that surprised you or gave unexpected results, why?
 
-//List 2 exprsssions you felt comfortable interpreting at a glance.
+//List 2 expressions you felt comfortable interpreting at a glance.
 
 //List 1 expression you are still 'shaky' on, why?
 
 
 // Task 2 - Practice JS Basics
+// 1 - 4 + 4 / 2
+const result = 4 + 4 / 2;
+
+// 2 - 
+
+// 3 - 
+
+// 4 - 
+
+// 5 - 
+
+// 6 - 
+
+// 7 - 
+
+// 8 - 
+
+// 9 -
+
+// 10 - 
+
+
+// Task 3 - Conditionals Practice 
+// 1 - 
+
+// 2 - 
+
+// 3 - 
+
+// 4 - 
+
+// 5 - 
+
+// 6 - 
+
+// 7 - 
+
+// 8 - 
+
+// 9 -
+
+// 10 - 
+
+// Task 4 - Conditionals Muscle Memory
 // 1 - 
 
 // 2 - 
@@ -53,7 +97,7 @@
 // 10 - 
 
 
-//// Task 3 - Conditionals Practice 
+// Task 5 - Function Muscle Memory
 // 1 - 
 
 // 2 - 
@@ -63,47 +107,3 @@
 // 4 - 
 
 // 5 - 
-
-// 6 - 
-
-// 7 - 
-
-// 8 - 
-
-// 9 -
-
-// 10 - 
-
-//// Task 4 - Conditionals Muscle Memory
-// 1 - 
-
-// 2 - 
-
-// 3 - 
-
-// 4 - 
-
-// 5 - 
-
-// 6 - 
-
-// 7 - 
-
-// 8 - 
-
-// 9 -
-
-// 10 - 
-
-
-//// Task 5 - Function Muscle Memory
-// 1 - 
-
-// 2 - 
-
-// 3 - 
-
-// 4 - 
-
-// 5 - 
-
