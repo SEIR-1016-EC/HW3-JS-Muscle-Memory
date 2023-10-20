15e96771bb7f116e9865a89bfcce7224537e6730
bsong1124 <bsong1124@gmail.com>
2023-10-20 12:07:19 -0700

    completed task 1

--- app.js
+++ app.js
@@ -2,87 +2,113 @@
 
 // List 3 expressions that surprised you or gave unexpected results, why?
 
+"true" === true; //false
+//Thought 'true' is a truthy statement which is equal to true
+"new york"[0]; //n
+"new york"[1]; //e
+"san francisco"[2 * 3]; //a
+"engineering".indexOf("G"); //-1
+//Didn't know you can use [] to find indeces of a string
+//Didn't know you can use dot notation on a string
+
 // List 2 exprsssions you felt comfortable interpreting at a glance.
+10 % 2 === 0; //true
+21 % 2 === 0; //false
 
 // List 1 expression you are still 'shaky' on, why?
-
-
-
-
+"true" === true; //false
+//Thought 'true' is a truthy statement which is equal to true
+/* === is an exact comparison so maybe because a truthy statemeny isnt EXACTLY equal to the 
+boolean of true it returns false */
 
 // Task 2 - Practice JS Basics
-// 1 - 
+// 1 -
+let idx = "abcde".indexOf("D");
+idx = idx + 11;
+console.log(idx); // 10
+idx * 2;
+console.log(idx); // 20
+/*The value of idx*2 does not change the value of idx becuase it isnt assigned a variable
+so my prediction was incorrect */
 
-// 2 - 
+// 2
+let num = 33;
+let isEven = num % 2 === 0;
+console.log(isEven); // false
+console.log(!isEven); // true
 
-// 3 - 
+// 3
+let str1 = 'marker';
+let num = 'whiteboard'.length - str1.length;
+console.log(num); // ?
+let str2 = 'bootcamp';
+console.log(str2[num].toUpperCase()); // 
+let char = str2[num].toLowerCase(); // ?
+console.log(char + '!'); // ?
 
-// 4 - 
+// 4 -
 
-// 5 - 
+// 5 -
 
-// 6 - 
+// 6 -
 
-// 7 - 
+// 7 -
 
-// 8 - 
-
-// 9 -
-
-// 10 - 
-
-
-//// Task 3 - Conditionals Practice 
-// 1 - 
-
-// 2 - 
-
-// 3 - 
-
-// 4 - 
-
-// 5 - 
-
-// 6 - 
-
-// 7 - 
-
-// 8 - 
+// 8 -
 
 // 9 -
 
-// 10 - 
+// 10 -
+
+//// Task 3 - Conditionals Practice
+// 1 -
+
+// 2 -
+
+// 3 -
+
+// 4 -
+
+// 5 -
+
+// 6 -
+
+// 7 -
+
+// 8 -
+
+// 9 -
+
+// 10 -
 
 //// Task 4 - Conditionals Muscle Memory
-// 1 - 
+// 1 -
 
-// 2 - 
+// 2 -
 
-// 3 - 
+// 3 -
 
-// 4 - 
+// 4 -
 
-// 5 - 
+// 5 -
 
-// 6 - 
+// 6 -
 
-// 7 - 
+// 7 -
 
-// 8 - 
+// 8 -
 
 // 9 -
 
-// 10 - 
-
+// 10 -
 
 //// Task 5 - Function Muscle Memory
-// 1 - 
+// 1 -
 
-// 2 - 
+// 2 -
 
-// 3 - 
+// 3 -
 
-// 4 - 
+// 4 -
 
-// 5 - 
-
+// 5 -
