1c83b77e136153e81bce39c741a15f0775c7c1da
roguione <myjobcontacts@yahoo.com>
2023-10-20 14:11:31 -0500

    notes

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-// Task 1 - Notes for JS Evaluation Predictions
+/* Task 1 - Notes for JS Evaluation Predictions
 
-// List 3 expressions that surprised you or gave unexpected results, why?
+    N1.  JavaScript Expressions: JavaScript expressions are evaluated from left to right 
+         based on operator precedence. 
 
-// List 2 exprsssions you felt comfortable interpreting at a glance.
+    N2.  Operator Precedence: JavaScript follows the order of operations (PEMDAS/BODMAS): 
+         Parentheses, Exponents, Multiplication/Division (left to right), 
+         Addition/Subtraction (left to right). 
+            1.   Parentheses
+            2.   Exponents
+            3.   Multiplication and Division (left to right)
+            4.   Addition and Subtraction (left to right) 
 
-// List 1 expression you are still 'shaky' on, why?
+    N3.  Evaluation Order: Understanding the order in which operators are applied 
+         is crucial for predicting the result of complex expressions
+
+    N4.  Use Parentheses: To explicitly control the order of evaluation, use parentheses. 
+                "For example, (2 + 2) / 2 would result in 2"
+   
+    N5.  Testing and Debugging: It's a good practice to test and debug your JavaScript code 
+         to ensure the expected results.
+
+    N6.  Documentation: Refer to JavaScript documentation or resources for detailed information 
+         on operator precedence and expressions. 
 
 
+//List 3 expressions that surprised you or gave unexpected results, why?
 
+//List 2 exprsssions you felt comfortable interpreting at a glance.
+
+//List 1 expression you are still 'shaky' on, why?
 
 
 // Task 2 - Practice JS Basics
