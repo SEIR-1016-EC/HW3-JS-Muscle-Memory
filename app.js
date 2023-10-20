2dee3a57ff69a21c62987836024f3da13599aeb7
honeybee <shelby.e.pagan@gmail.com>
2023-10-20 17:17:43 -0400

    task1 complete

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 // Task 1 - Notes for JS Evaluation Predictions
 
 // List 3 expressions that surprised you or gave unexpected results, why?
-
+    /* 
+    2 + 'pizza' = '2pizza' ????
+    5 === 5.0 = true. I thought this would be false because of the decimal point
+    'new york'[0] = 'n' this gives you the letter in the 0 index position 
+    */
 // List 2 exprsssions you felt comfortable interpreting at a glance.
-
+    /* 
+    4 + 4 / 2
+    (2 + -7) * 3 
+    */
 // List 1 expression you are still 'shaky' on, why?
+    /* 'science'.indexOf('x') === -1 WHAT DOES THIS MEAN??? Why does any letter not included in the string = -1
+    */
 
 
 
@@ -32,7 +41,7 @@
 // 10 - 
 
 
-//// Task 3 - Conditionals Practice 
+// Task 3 - Conditionals Practice 
 // 1 - 
 
 // 2 - 
@@ -53,7 +62,7 @@
 
 // 10 - 
 
-//// Task 4 - Conditionals Muscle Memory
+// Task 4 - Conditionals Muscle Memory
 // 1 - 
 
 // 2 - 
@@ -75,7 +84,7 @@
 // 10 - 
 
 
-//// Task 5 - Function Muscle Memory
+// Task 5 - Function Muscle Memory
 // 1 - 
 
 // 2 - 
