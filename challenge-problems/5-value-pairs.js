2790f66c38ecc4f9e537b83e370946d3b18dad38
Parth Vachhani <vachhani.parth.96@gmail.com>
2023-10-21 15:38:05 -0400

    Completed challenge 5

--- challenge-problems/5-value-pairs.js
+++ challenge-problems/5-value-pairs.js
@@ -14,6 +14,22 @@ valuePair(object1, object2, 'name'); // => [ 'One', 'Two' ]
 
 function valuePair(obj1, obj2, key) {
   // your code here
+  let object1 = obj1;
+  let object2 = obj2;
+  let objKey = key;
+  let obj1Value;
+  let obj2Value;
+    for(let objKey in obj1) {
+      obj1Value = obj1[key];
+    }
+    for(let objKkey in obj2) {
+      obj2Value = obj2[key];
+    }
+    return [obj1Value, obj2Value];
 }
+const object1 = {name: 'One', location: 'Remote', age: 1};
+const object2 = {name: 'Two', location: 'San Francisco'};
 
+// console.log(valuePair(object1, object2, 'location'));
+// console.log(valuePair(object1, object2, 'name'));
 module.exports = valuePair;
