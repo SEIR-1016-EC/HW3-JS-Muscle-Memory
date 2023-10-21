37b2205957562e23ba1fcde1f920c667c0a30897
BrObando <brianna.obando@gmail.com>
2023-10-21 13:38:06 -0400

    Task1

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
 // Task 1 - Notes for JS Evaluation Predictions
 
+4 + 4 / 2 .  // 8
+(2 + -7) * 3
+101 % 10
+12 - 4 % 3
+true && false
+true && !(false || false)
+!true && !(false || false)
+'cat' + 'dog'
+2 + 'pizza'
+2.5 * 'fish'
+5 >= 11
+5 === 5.0
+7 !== 7.1
+5 + 5 > 8
+6 + 6 !== 12
+2 > 1 || false
+'true' === true
+10 % 2 === 0
+21 % 2 === 0
+21 % 2 !== 0
+21 % 2 === 1
+12 % 3 === 0
+9 % 3 === 0
+14 % 3 === 0
+'new york'[0]
+'new york'[1]
+'san francisco'[2 * 3]
+'engineering'[3].toUpperCase()
+'engineering'.indexOf('G')
+'engineering'.indexOf('neer')
+'engineering'.indexOf('r') > -1
+'science'.indexOf('x') === -1
+
 // List 3 expressions that surprised you or gave unexpected results, why?
 
 // List 2 exprsssions you felt comfortable interpreting at a glance.
