061b29ea85dfd62aacb758863ac17da57d72a0c4
Parth Vachhani <vachhani.parth.96@gmail.com>
2023-10-21 14:34:31 -0400

    Completed Task 5, now my fingers are cramping.

--- app.js
+++ app.js
@@ -332,12 +332,427 @@ if (num < 0) {
 
 //// Task 5 - Function Muscle Memory
 // 1 - Prediction: 
+function sayHello(name) {
+    let msg = 'Hello ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('bootcamp prep'));
 
+function sayHello(name) {
+    let msg = 'Hello ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('bootcamp prep'));
+
+function sayHello(name) {
+    let msg = 'Hello ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('bootcamp prep'));
+
+function sayHello(name) {
+    let msg = 'Hello ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('bootcamp prep'));
+
+function sayHello(name) {
+    let msg = 'Hello ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('bootcamp prep'));
+
+function sayHello(name) {
+    let msg = 'Hello ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('bootcamp prep'));
+
+function sayHello(name) {
+    let msg = 'Hello ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('bootcamp prep'));
+
+function sayHello(name) {
+    let msg = 'Hello ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('bootcamp prep'));
+
+function sayHello(name) {
+    let msg = 'Hello ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('bootcamp prep'));
+
+function sayHello(name) {
+    let msg = 'Hello ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('bootcamp prep'));
 // 2 - Prediction: 
+function checkNumber(num) {
+    if(num>0) {
+        return 'positive';
+    } else if(num<0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(5));
+
+function checkNumber(num) {
+    if(num>0) {
+        return 'positive';
+    } else if(num<0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(5));
+
+function checkNumber(num) {
+    if(num>0) {
+        return 'positive';
+    } else if(num<0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(5));
+
+function checkNumber(num) {
+    if(num>0) {
+        return 'positive';
+    } else if(num<0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(5));
+
+function checkNumber(num) {
+    if(num>0) {
+        return 'positive';
+    } else if(num<0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(5));
+
+function checkNumber(num) {
+    if(num>0) {
+        return 'positive';
+    } else if(num<0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(5));
+
+function checkNumber(num) {
+    if(num>0) {
+        return 'positive';
+    } else if(num<0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(5));
+
+function checkNumber(num) {
+    if(num>0) {
+        return 'positive';
+    } else if(num<0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(5));
+
+function checkNumber(num) {
+    if(num>0) {
+        return 'positive';
+    } else if(num<0) {
+        return 'negative';
+    } else {
+        return 'zero'
+    }
+}
+console.log(checkNumber(5));
+
+function checkNumber(num) {
+    if(num>0) {
+        return 'positive';
+    } else if(num<0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(5));
 
 // 3 - Prediction: 
+function fizzbuzz1(max) {
+    for (let i = 0; i < max; i+=1) {
+        if(i%3 === 0 && i%5 !== 0){
+            console.log(i);
+        } else if(i%5 === 0 && i%3 !== 0) {
+            console.log(i);
+        }
+    }
+}
+
+function fizzbuzz1(max) {
+    for(let i = 0; i<max; i+=1) {
+        if(i%3 === 0 && i%5 !== 0) {
+            console.log(i);
+        }else if(i%5 === 0 && i%3 !== 0) {
+            console.log(i);
+        }
+    }
+}
+
+function fizzbuzz1(max) {
+    for(let i=0; i<max; i+=1) {
+        if(i%3 === 0 && i%5 !== 0) {
+            console.log(i);
+        } else if(i%5 === 0 && i%3 !== 0) {
+            console.log(i);
+        }
+    }
+}
+
+function fizzbuzz1(max) {
+    for(let i=0; i<max; i+=1) {
+        if(i%3 === 0 && i%5 !== 0) {
+            console.log(i);
+        } else if(i%5 === 0 && i%3 !== 0) {
+            console.log(i);
+        }
+    }
+}
+
+function fizzbuzz1(max) {
+    for(let i=0; i<max; i+=1) {
+        if(i%3 === 0 && i%5 !== 0) {
+            console.log(i);
+        } else if(i%5 === 0 && i%3 !== 0) {
+            console.log(i);
+        }
+    }
+}
+
+function fizzbuzz1(max) {
+    for(let i=0; i<max; i+=1) {
+        if(i%3 === 0 && i%5 !== 0) {
+            console.log(i);
+        } else if(i%5 === 0 && i%3 !== 0) {
+            console.log(i);
+        }
+    }
+}
+
+function fizzbuzz1(max) {
+    for(let i=0; i<max; i+=1) {
+        if(i%3 === 0 && i%5 !== 0) {
+            console.log(i);
+        } else if(i%5 === 0 && i%3 !== 0) {
+            console.log(i);
+        }
+    }
+}
+
+function fizzbuzz1(max) {
+    for(let i=0; i<max; i+=1) {
+        if(i%3 === 0 && i%5 !== 0) {
+            console.log(i);
+        } else if(i%5 === 0 && i%3 !== 0) {
+            console.log(i);
+        }
+    }
+}
+
+function fizzbuzz1(max) {
+    for(let i=0; i<max; i+=1) {
+        if(i%3 === 0 && i%5 !== 0) {
+            console.log(i);
+        } else if(i%5 === 0 && i%3 !== 0) {
+            console.log(i);
+        }
+    }
+}
+
+function fizzbuzz1(max) {
+    for(let i=0; i<max; i+=1) {
+        if(i%3 === 0 && i%5 !== 0) {
+            console.log(i);
+        } else if(i%5 === 0 && i%3 !== 0) {
+            console.log(i);
+        }
+    }
+}
 
 // 4 - Prediction: 
+function evenCaps(sentence) {
+    let newSentence = "";
+    for (let i = 0; i < sentence.length; i++) {
+        let char = sentence[i];
+
+        if (i % 2 === 0) {
+        let capitalChar = char.toUpperCase();
+        newSentence += capitalChar;
+        } else {
+        newSentence += char;
+        }
+    }
+    return newSentence;
+}
+
+function evenCaps(sentence) {
+    let newSentence = "";
+    for (let i = 0; i < sentence.length; i++) {
+        let char = sentence[i];
+        
+        if (i % 2 === 0) {
+        let capitalChar = char.toUpperCase();
+        newSentence += capitalChar;
+        } else {
+        newSentence += char;
+        }
+    }
+    return newSentence;
+}
+
+function evenCaps(sentence) {
+    let newSentence = "";
+    for (let i = 0; i < sentence.length; i++) {
+        let char = sentence[i];
+        
+        if (i % 2 === 0) {
+        let capitalChar = char.toUpperCase();
+        newSentence += capitalChar;
+        } else {
+        newSentence += char;
+        }
+    }
+    return newSentence;
+}
+
+function evenCaps(sentence) {
+    let newSentence = "";
+    for (let i = 0; i < sentence.length; i++) {
+        let char = sentence[i];
+        
+        if (i % 2 === 0) {
+        let capitalChar = char.toUpperCase();
+        newSentence += capitalChar;
+        } else {
+        newSentence += char;
+        }
+    }
+    return newSentence;
+}
+
+function evenCaps(sentence) {
+    let newSentence = "";
+    for (let i = 0; i < sentence.length; i++) {
+        let char = sentence[i];
+        
+        if (i % 2 === 0) {
+        let capitalChar = char.toUpperCase();
+        newSentence += capitalChar;
+        } else {
+        newSentence += char;
+        }
+    }
+    return newSentence;
+}
+
+function evenCaps(sentence) {
+    let newSentence = "";
+    for (let i = 0; i < sentence.length; i++) {
+        let char = sentence[i];
+        
+        if (i % 2 === 0) {
+        let capitalChar = char.toUpperCase();
+        newSentence += capitalChar;
+        } else {
+        newSentence += char;
+        }
+    }
+    return newSentence;
+}
+
+function evenCaps(sentence) {
+    let newSentence = "";
+    for (let i = 0; i < sentence.length; i++) {
+        let char = sentence[i];
+        
+        if (i % 2 === 0) {
+        let capitalChar = char.toUpperCase();
+        newSentence += capitalChar;
+        } else {
+        newSentence += char;
+        }
+    }
+    return newSentence;
+}
+
+function evenCaps(sentence) {
+    let newSentence = "";
+    for (let i = 0; i < sentence.length; i++) {
+        let char = sentence[i];
+        
+        if (i % 2 === 0) {
+        let capitalChar = char.toUpperCase();
+        newSentence += capitalChar;
+        } else {
+        newSentence += char;
+        }
+    }
+    return newSentence;
+}
+
+function evenCaps(sentence) {
+    let newSentence = "";
+    for (let i = 0; i < sentence.length; i++) {
+        let char = sentence[i];
+        
+        if (i % 2 === 0) {
+        let capitalChar = char.toUpperCase();
+        newSentence += capitalChar;
+        } else {
+        newSentence += char;
+        }
+    }
+    return newSentence;
+}
+
+function evenCaps(sentence) {
+    let newSentence = "";
+    for (let i = 0; i < sentence.length; i++) {
+        let char = sentence[i];
+        
+        if (i % 2 === 0) {
+        let capitalChar = char.toUpperCase();
+        newSentence += capitalChar;
+        } else {
+        newSentence += char;
+        }
+    }
+    return newSentence;
+}
 
 // 5 - Prediction: 
 
