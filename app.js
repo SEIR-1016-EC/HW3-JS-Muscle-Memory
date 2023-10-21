0a83adfe20cd287b4f3eb2f34cc80c931e07c725
Connor Gunter <connorgunter@Connors-MBP.maine.rr.com>
2023-10-21 13:29:14 -0400

    completed task 3

--- app.js
+++ app.js
@@ -33,31 +33,39 @@
 // 'science'.indexOf('x') === -1 -> true
 
 // List 3 expressions that surprised you or gave unexpected results, why?
-//2 + 'pizza' surpised me because I figured it would have been undefined, my thought process was you cant add a string a number together
+// 2 + 'pizza' surpised me because I figured it would have been undefined, my thought process was you cant add a string a number together
 // 6 + 6 !== 12 this one surprised me because 6+6 = 12 im still kind of confused on it
 // 'engineering'.indexOf('G') -> -1 i figured this one would output [2,10]
+
 // List 2 exprsssions you felt comfortable interpreting at a glance.
 // (2 + -7) * 3 -> 15
 // 101 % 10 -> 1
+
 // List 1 expression you are still 'shaky' on, why?
-//science'.indexOf('x') === -1 -> true i dont undsertand how this equates to true
+// science'.indexOf('x') === -1 -> true i dont undsertand how this equates to true
 
 
 
 
 // Task 2 - Practice JS Basics
+
 // 1 - 
+
 //let idx = 'abcde'.indexOf('D');
 //idx = idx + 11;
 //console.log(idx); //10 
 //idx * 2;
 //console.log(idx); //20 -> actually printed 10
+
 // 2 - 
+
 //let num =33
 //let isEven = num % 2 === 0
 //console.log(isEven) // false
 //console.log(!isEven) // true
+
 // 3 - 
+
 // let str1 = 'marker'
 // let num = 'whiteboard'.length - str1.length
 // console.log(num) // 4
@@ -65,25 +73,97 @@
 // console.log(str2[num].toUpperCase()) // C
 // let char =str2[num].toLowerCase()
 // console.log(char + '!') // c!
+
 // 4- 
+
 // let sentence = 'welcome to bootcamp prep'
 // let lastChar = sentence[sentence.length -1]
 // console.log(lastChar) // p
 // console.log(sentence.indexOf(lastChar)) // 20 -> actually printed 18
+
 //// Task 3 - Conditionals Practice 
+
 // 1 - 
+
+// let age = 56
+// if (age > 30) {
+//     console.log('older than 30')
+// } else {
+//     console.log('younger than 30')
+// }
+// age = 30 -> younger than 30
+// age 56 -> older than 30
 
 // 2 - 
 
+// let str = 'private party'
+// if (str.length > 10) {
+//     console.log('long string')
+// } else {
+//     console.log('short string')
+// }
+// if (str[0] === 'p') {
+//     console.log('starts with p')
+// }
+// str = 'pizza' -> short string, starts with p
+// str = 'connor -> short string
+// str = 'private party' -> long string, starts with p
+
 // 3 - 
+
+// let num = 10
+// if (num % 3 === 0) {
+// console.log('divisable by 3')
+// }else if (num % 5 === 0) {
+//     console.log('divisable by 5')
+// }
+// num = 15 -> divisable by 3
+// num = 10 -> divisable by 5
 
 // 4 - 
 
+// let num = 37
+// if (num % 3 === 0) {
+//     console.log('divisable by 3')
+// }
+// if (num % 5 === 0) {
+//     console.log('divisable by 5')
+// }
+// num = 15 -> divisable by 3, divisable by 5
+// num = 37 -> false -> actually printed nothing
+
 // 5 - 
+
+// let str = 'General AssemblY'
+// if (str[0] === str[0].toUpperCase()) {
+//     console.log('starts with capital!')
+// }
+// if (str[str.length-1] === str[str.length-1].toUpperCase()) {
+//     console.log('ends with a capital!')
+// }
+// str = 'General Assembly' -> starts with a capital
+// str = 'General AssemblY -> starts with a capital, ends with a capital
 
 // 6 -
 
+// let num =  53
+// if (num >0) {
+//     console.log('positive')
+// } else if (num < 0){
+//     console.log('negative')
+// } else {
+//     console.log(0)
+// }
+// if (num % 2 === 0) {
+//     console.log('even')
+// }else {
+//     console.log('odd')
+// }
+// num = -44 -> negative, even
+// num = 53 -> positive, odd
+
 //// Task 4 - Conditionals Muscle Memory
+
 // 1 - 
 
 // 2 - 
