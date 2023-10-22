c6872bbc71f516a2afb569f7756885b31e6a2c2d
Andrew Brown <drewkyle01@gmail.com>
2023-10-22 13:42:27 -0400

    Task 4 Complete

---
+++ HW3-JS-Muscle-Memory/app.js
@@ -0,0 +1,262 @@
+// Task 1 - Notes for JS Evaluation Predictions
+
+// List 3 expressions that surprised you or gave unexpected results, why?
+
+// List 2 exprsssions you felt comfortable interpreting at a glance.
+
+// List 1 expression you are still 'shaky' on, why?
+
+// Task 2 - Practice JS Basics
+// 1 -
+
+// 2 -
+
+// 3 -
+
+// 4 -
+
+// 5 -
+
+// 6 -
+
+// 7 -
+
+// 8 -
+
+// 9 -
+
+// 10 -
+
+//// Task 3 - Conditionals Practice
+// 1 -
+
+// 2 -
+
+// 3 -
+
+// 4 -
+
+// 5 -
+
+// 6 -
+
+// 7 -
+
+// 8 -
+
+// 9 -
+
+// 10 -
+
+//// Task 4 - Conditionals Muscle Memory
+
+if (num > 5) {
+  console.log("if");
+}
+if (num > 5) {
+  console.log("if");
+}
+if (num > 5) {
+  console.log("if");
+}
+if (num > 5) {
+  console.log("if");
+}
+if (num > 5) {
+  console.log("if");
+}
+if (num > 5) {
+  console.log("if");
+}
+if (num > 5) {
+  console.log("if");
+}
+if (num > 5) {
+  console.log("if");
+}
+if (num > 5) {
+  console.log("if");
+}
+if (num > 5) {
+  console.log("if");
+}
+// THis one wasn't too bad!!
+
+// 2 -
+let num = 5;
+if (num > 5) {
+  console.log("if");
+} else {
+  console.log("else");
+}
+
+let num = 5;
+if (num > 5) {
+  console.log("if");
+} else {
+  console.log("else");
+}
+
+let num = 5;
+if (num > 5) {
+  console.log("if");
+} else {
+  console.log("else");
+}
+
+let num = 5;
+if (num > 5) {
+  console.log("if");
+} else {
+  console.log("else");
+}
+
+let num = 5;
+if (num > 5) {
+  console.log("if");
+} else {
+  console.log("else");
+}
+
+let num = 5;
+if (num > 5) {
+  console.log("if");
+} else {
+  console.log("else");
+}
+
+let num = 5;
+if (num > 5) {
+  console.log("if");
+} else {
+  console.log("else");
+}
+
+let num = 5;
+if (num > 5) {
+  console.log("if");
+} else {
+  console.log("else");
+}
+
+let num = 5;
+if (num > 5) {
+  console.log("if");
+} else {
+  console.log("else");
+}
+
+let num = 5;
+if (num > 5) {
+  console.log("if");
+} else {
+  console.log("else");
+}
+// Also not too bad
+
+// 3 -
+let num = 0;
+if (num < 0) {
+  console.log("if");
+} else if (num > 0) {
+  console.log("else if");
+} else {
+  console.log("else");
+}
+let num = 0;
+if (num < 0) {
+  console.log("if");
+} else if (num > 0) {
+  console.log("else if");
+} else {
+  console.log("else");
+}
+let num = 0;
+if (num < 0) {
+  console.log("if");
+} else if (num > 0) {
+  console.log("else if");
+} else {
+  console.log("else");
+}
+let num = 0;
+if (num < 0) {
+  console.log("if");
+} else if (num > 0) {
+  console.log("else if");
+} else {
+  console.log("else");
+}
+let num = 0;
+if (num < 0) {
+  console.log("if");
+} else if (num > 0) {
+  console.log("else if");
+} else {
+  console.log("else");
+}
+let num = 0;
+if (num < 0) {
+  console.log("if");
+} else if (num > 0) {
+  console.log("else if");
+} else {
+  console.log("else");
+}
+let num = 0;
+if (num < 0) {
+  console.log("if");
+} else if (num > 0) {
+  console.log("else if");
+} else {
+  console.log("else");
+}
+let num = 0;
+if (num < 0) {
+  console.log("if");
+} else if (num > 0) {
+  console.log("else if");
+} else {
+  console.log("else");
+}
+let num = 0;
+if (num < 0) {
+  console.log("if");
+} else if (num > 0) {
+  console.log("else if");
+} else {
+  console.log("else");
+}
+let num = 0;
+if (num < 0) {
+  console.log("if");
+} else if (num > 0) {
+  console.log("else if");
+} else {
+  console.log("else");
+}
+// I started getting a headache at this point.. I would go back and look to see what I mistyped and it would take forever to see it.
+// 4 -
+
+// 5 -
+
+// 6 -
+
+// 7 -
+
+// 8 -
+
+// 9 -
+
+// 10 -
+
+//// Task 5 - Function Muscle Memory
+// 1 -
+
+// 2 -
+
+// 3 -
+
+// 4 -
+
+// 5 -
