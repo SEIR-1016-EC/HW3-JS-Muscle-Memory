d743ccd59dcfb6a1010e20bccff32073d4cda018
chrisr5440 <cramirez5440@gmail.com>
2023-10-22 16:46:49 -0400

    task 2 complete

--- app.js
+++ app.js
@@ -9,25 +9,48 @@
 
 
 // List 2 exprsssions you felt comfortable interpreting at a glance.
+/* 
+4 + 4 /2 
+(2 = -7) *3
+*/
 
 // List 1 expression you are still 'shaky' on, why?
-
+'resizing'.indexOf('size')// I assumed it returns where this starts and in the index, but why not a range of letters or words??????
 
 
 
 
 // Task 2 - Practice JS Basics
-// 1 - 
+const isTrue = true;
+const isFalse = false;
+//1
 
-// 2 - 
+let count = 23;
+newCount = count + 8;
+console.log(newCount);
+//2
 
-// 3 - 
+const message = 'Hello' + 'People!';
+//3 
 
-// 4 - 
+let score = 50;
+score = 25;
+console.log(score);
+//4
 
-// 5 - 
+const num = 30;
+if (num > 30) {
+    console.log("Guess smaller!");
+if (num < 30) {
+    console.log("Guess Bigger!");
+    else {
+        console.log("You Won!")
+    }}}
+//5
 
-// 6 - 
+for (let i=0; i < 25; i--){
+    console.log(i);}
+    //6 
 
 // 7 - 
 
