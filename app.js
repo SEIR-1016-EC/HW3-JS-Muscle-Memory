958a1af1e959d9301380d22d69240e8067ead0cc
Branko <branko.stanisic02@gmail.com>
2023-10-22 15:07:52 -0400

    Complete HW3

--- app.js
+++ app.js
@@ -1,36 +1,62 @@
 // Task 1 - Notes for JS Evaluation Predictions
 
 // List 3 expressions that surprised you or gave unexpected results, why?
+//Equality operations: true, false !=== null etc; pretty confusing
+//i%5 meaning that 'i' is a multiple of 5, interseting that we use a % simbol
+//arrays; too hard and sometimes not able to find the right answer
 
 // List 2 exprsssions you felt comfortable interpreting at a glance.
-
+//let i = 0; i <= 100; i++
+//finding a multiple of 3 is i%3 === 0
 // List 1 expression you are still 'shaky' on, why?
-
+//a lot of JS, almost everything; arrays, console.log, !== => etc
 
 
 
 
 // Task 2 - Practice JS Basics
 // 1 - 
-
+let score = 100;
+score = 25;
 // 2 - 
-
+const months = ['Jan', 'Feb', 'March', 'April'];
+months.splice(1. 0, 'Feb');
+console.log(months)
 // 3 - 
-
+function sayHelloWorld() {
+    console('HelloWorld');
+}
 // 4 - 
-
+const num = 30;
+if (num > 30) {
+    console.log("Guess lower!");
+if (num < 30) {
+    console.log("Guess higher!");
+else {
+    console.log("You guessed the number!")
+}}}
 // 5 - 
-
+for (let i = 0; i <=100; i+=2) {
+console.log(i);}
 // 6 - 
-
+for (let i=0; i <50; i--){
+console.log(i);}
 // 7 - 
-
+const aboutMe = {
+    name: 'Branko';
+    age: 21;
+    hobbies: 'basketball', 'running';
+    console.log(aboutMe);
+}
 // 8 - 
-
+const isTrue = true;
+const isFalse = false;
 // 9 -
-
+const message = 'Hello' + 'World!';
 // 10 - 
-
+let count = 23;
+newCount = count + 8;
+console.log(newCount);
 
 //// Task 3 - Conditionals Practice 
 // 1 - 
