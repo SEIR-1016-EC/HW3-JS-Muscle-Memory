738fe75de25b063518909540f0e2d82291731031
Andrew Brown <drewkyle01@gmail.com>
2023-10-22 13:52:21 -0400

    Task 5 Update

--- HW3-JS-Muscle-Memory/app.js
+++ HW3-JS-Muscle-Memory/app.js
@@ -516,5 +516,274 @@ function checkNumber(num) {
 console.log(checkNumber(5));
 // SOS at this point ... this one took me the longest
 // 4 -
+function fizzBuzz1(max) {
+  for (let i = 0; i < max; i += 1) {
+    if (i % 3 === 0 && i % 5 !== 0) {
+      console.log(i);
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+      console.log(i);
+    }
+  }
+}
 
+function fizzBuzz1(max) {
+  for (let i = 0; i < max; i += 1) {
+    if (i % 3 === 0 && i % 5 !== 0) {
+      console.log(i);
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+      console.log(i);
+    }
+  }
+}
+
+function fizzBuzz1(max) {
+  for (let i = 0; i < max; i += 1) {
+    if (i % 3 === 0 && i % 5 !== 0) {
+      console.log(i);
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+      console.log(i);
+    }
+  }
+}
+
+function fizzBuzz1(max) {
+  for (let i = 0; i < max; i += 1) {
+    if (i % 3 === 0 && i % 5 !== 0) {
+      console.log(i);
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+      console.log(i);
+    }
+  }
+}
+
+function fizzBuzz1(max) {
+  for (let i = 0; i < max; i += 1) {
+    if (i % 3 === 0 && i % 5 !== 0) {
+      console.log(i);
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+      console.log(i);
+    }
+  }
+}
+
+function fizzBuzz1(max) {
+  for (let i = 0; i < max; i += 1) {
+    if (i % 3 === 0 && i % 5 !== 0) {
+      console.log(i);
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+      console.log(i);
+    }
+  }
+}
+
+function fizzBuzz1(max) {
+  for (let i = 0; i < max; i += 1) {
+    if (i % 3 === 0 && i % 5 !== 0) {
+      console.log(i);
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+      console.log(i);
+    }
+  }
+}
+
+function fizzBuzz1(max) {
+  for (let i = 0; i < max; i += 1) {
+    if (i % 3 === 0 && i % 5 !== 0) {
+      console.log(i);
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+      console.log(i);
+    }
+  }
+}
+
+function fizzBuzz1(max) {
+  for (let i = 0; i < max; i += 1) {
+    if (i % 3 === 0 && i % 5 !== 0) {
+      console.log(i);
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+      console.log(i);
+    }
+  }
+}
+
+function fizzBuzz1(max) {
+  for (let i = 0; i < max; i += 1) {
+    if (i % 3 === 0 && i % 5 !== 0) {
+      console.log(i);
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+      console.log(i);
+    }
+  }
+}
+// Had to take a break after this one lol
 // 5 -
+function evenCaps(sentence) {
+  let newSentence = "";
+
+  for (let i = 0; i < sentence.length; i++) {
+    let char = sentence[i];
+
+    if (i % 2 === 0) {
+      let capitalChar = char.toUpperCase();
+      newSentence += capitalChar;
+    } else {
+      newSentence += char;
+    }
+  }
+
+  return newSentence;
+}
+
+function evenCaps(sentence) {
+  let newSentence = "";
+
+  for (let i = 0; i < sentence.length; i++) {
+    let char = sentence[i];
+
+    if (i % 2 === 0) {
+      let capitalChar = char.toUpperCase();
+      newSentence += capitalChar;
+    } else {
+      newSentence += char;
+    }
+  }
+
+  return newSentence;
+}
+
+function evenCaps(sentence) {
+  let newSentence = "";
+
+  for (let i = 0; i < sentence.length; i++) {
+    let char = sentence[i];
+
+    if (i % 2 === 0) {
+      let capitalChar = char.toUpperCase();
+      newSentence += capitalChar;
+    } else {
+      newSentence += char;
+    }
+  }
+
+  return newSentence;
+}
+
+function evenCaps(sentence) {
+  let newSentence = "";
+
+  for (let i = 0; i < sentence.length; i++) {
+    let char = sentence[i];
+
+    if (i % 2 === 0) {
+      let capitalChar = char.toUpperCase();
+      newSentence += capitalChar;
+    } else {
+      newSentence += char;
+    }
+  }
+
+  return newSentence;
+}
+
+function evenCaps(sentence) {
+  let newSentence = "";
+
+  for (let i = 0; i < sentence.length; i++) {
+    let char = sentence[i];
+
+    if (i % 2 === 0) {
+      let capitalChar = char.toUpperCase();
+      newSentence += capitalChar;
+    } else {
+      newSentence += char;
+    }
+  }
+
+  return newSentence;
+}
+
+function evenCaps(sentence) {
+  let newSentence = "";
+
+  for (let i = 0; i < sentence.length; i++) {
+    let char = sentence[i];
+
+    if (i % 2 === 0) {
+      let capitalChar = char.toUpperCase();
+      newSentence += capitalChar;
+    } else {
+      newSentence += char;
+    }
+  }
+
+  return newSentence;
+}
+
+function evenCaps(sentence) {
+  let newSentence = "";
+
+  for (let i = 0; i < sentence.length; i++) {
+    let char = sentence[i];
+
+    if (i % 2 === 0) {
+      let capitalChar = char.toUpperCase();
+      newSentence += capitalChar;
+    } else {
+      newSentence += char;
+    }
+  }
+
+  return newSentence;
+}
+
+function evenCaps(sentence) {
+  let newSentence = "";
+
+  for (let i = 0; i < sentence.length; i++) {
+    let char = sentence[i];
+
+    if (i % 2 === 0) {
+      let capitalChar = char.toUpperCase();
+      newSentence += capitalChar;
+    } else {
+      newSentence += char;
+    }
+  }
+
+  return newSentence;
+}
+
+function evenCaps(sentence) {
+  let newSentence = "";
+
+  for (let i = 0; i < sentence.length; i++) {
+    let char = sentence[i];
+
+    if (i % 2 === 0) {
+      let capitalChar = char.toUpperCase();
+      newSentence += capitalChar;
+    } else {
+      newSentence += char;
+    }
+  }
+
+  return newSentence;
+}
+
+function evenCaps(sentence) {
+  let newSentence = "";
+
+  for (let i = 0; i < sentence.length; i++) {
+    let char = sentence[i];
+
+    if (i % 2 === 0) {
+      let capitalChar = char.toUpperCase();
+      newSentence += capitalChar;
+    } else {
+      newSentence += char;
+    }
+  }
+
+  return newSentence;
+}
+// I went to bad after finishing this I was totally wiped out
