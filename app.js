b7b3ec845217360cc1a3fd39d7b155c59d0abcf6
wquynguy <wquyenle@gmail.com>
2023-10-23 07:06:32 -0500

    Task 3

--- app.js
+++ app.js
@@ -67,23 +67,86 @@ console.log(sentence.indexOf(lastChar));
 //// Task 3 - Conditionals Practice 
 // 1 - 
 
+let age = 30; // try different numbers here
+if (age > 30) {
+  console.log('older than 30');
+} else {
+  console.log('younger than 30');
+}
+
+console.log(90);
+console.log(40);
+
+
+
 // 2 - 
 
-// 3 - 
+let str = 'pizza'; 
+if (str.length > 10) {
+  console.log('long string');
+} else {
+  console.log('short string');
+}
+if (str[0] === 'p') {
+  console.log('starts with p');
+}
 
-// 4 - 
+console.log('Cream');
+
+// 3 - 
+let num = 15; 
+if (num % 3 === 0) {
+  console.log('divisible by 3');
+} else if (num % 5 === 0) {
+  console.log('divisible by 5');
+}
+
+console.log(90);
+
+// 4 -
+
+let num = 15; 
+if (num % 3 === 0) {
+  console.log('divisible by 3');
+}
+if (num % 5 === 0) {
+  console.log('divisible by 5');
+}
+
+console.log(52);
+
 
 // 5 - 
 
-// 6 - 
+let str = 'General Assembly'; 
 
-// 7 - 
+if (str[0] === str[0].toUpperCase()) {
+  console.log('starts with a capital!');
+}
+if (str[str.length - 1] === str[str.length - 1].toUpperCase()) {
+  console.log('ends with a capital!');
+}
 
-// 8 - 
+console.log('General Assembly');
 
-// 9 -
+// 6 -
 
-// 10 - 
+let num = -44; 
+if (num > 0) {
+  console.log('positive');
+} else if (num < 0) {
+  console.log('negative');
+} else {
+  console.log(0);
+}
+if (num % 2 === 0) {
+  console.log('even');
+} else {
+  console.log('odd');
+}
+
+console.log(25);
+
 
 //// Task 4 - Conditionals Muscle Memory
 // 1 - 
