6315cb4e9835fbea5bdb6ddd98b34e955abc89ad
wquynguy <wquyenle@gmail.com>
2023-10-23 07:11:01 -0500

    Task 4 - 6

--- app.js
+++ app.js
@@ -149,35 +149,91 @@ console.log(25);
 
 
 //// Task 4 - Conditionals Muscle Memory
-// 1 - 
+// 1 -  
+let num = 15; 
+if (num > 5) {
+    console.log('if');
+  }
 
-// 2 - 
+  console.log(100);
+
+// 2 -  
+let num = 15; 
+if (num > 5) {
+    console.log('if');
+  } else {
+    console.log('else');
+  }
+
+  console.log(80);
 
 // 3 - 
+let num = 20; 
+if (num < 0) {
+    console.log('if');
+  } else if (num > 0) {
+    console.log('else if');
+  } else {
+    console.log('else');
+  }
 
-// 4 - 
-
-// 5 - 
-
-// 6 - 
-
-// 7 - 
-
-// 8 - 
-
-// 9 -
-
-// 10 - 
+  console.log(250);
 
 
 //// Task 5 - Function Muscle Memory
 // 1 - 
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+  }
+  console.log(sayHello('your majesty')); 
 
 // 2 - 
 
+function checkNumber(num) {
+    if (num > 0) {
+      return 'positive';
+    } else if (num < 0) {
+      return 'negative';
+    } else {
+      return 'zero';
+    }
+  }
+  console.log(checkNumber(25)); 
+  
+
 // 3 - 
+
+function fizzBuzz1(max) {
+    for (let i = 0; i < max; i += 1) {
+  
+      if (i % 3 === 0 && i % 5 !== 0) {
+        console.log(i);
+      } else if (i % 5 === 0 && i % 3 !== 0) {
+        console.log(i);
+      }
+    }
+  }
+
 
 // 4 - 
 
-// 5 - 
+function evenCaps(sentence) {
+    let newSentence = "";
+  
+    for (let i = 0; i < sentence.length; i++) {
+      let char = sentence[i];
+  
+      if (i % 2 === 0) {
+        let capitalChar = char.toUpperCase();
+        newSentence += capitalChar;
+      } else {
+        newSentence += char;
+      }
+    }
+  
+    return newSentence;
+  }
+
+
 
