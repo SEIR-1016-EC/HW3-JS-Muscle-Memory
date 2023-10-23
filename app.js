7ceb9fb488b9743bb476f2dba3f7814a7e8b20e5
User Name <redd@Gilbertos-MBP.lan>
2023-10-23 07:25:34 -0500

    Not complete

--- app.js
+++ app.js
@@ -2,20 +2,50 @@
 
 // List 3 expressions that surprised you or gave unexpected results, why?
 
+
+'new york'[0]; 
+'new york'[1];           // These three. Didn't know it would extract a single character from the index of the string.
+'san francisco'[2 * 3];
+
+
 // List 2 exprsssions you felt comfortable interpreting at a glance.
+
+
+(2 + -7) * 3;
+'cat' + 'dog';
+
 
 // List 1 expression you are still 'shaky' on, why?
 
-
+'science'.indexOf('x') === -1 // I was baffled and didn't know why i was geting truthy. Couldn't make sense of it and looked it up. (now i understand)
 
 
 
 // Task 2 - Practice JS Basics
 // 1 - 
+    
+let idx = 'abcde'.indexOf('D'); // No 'D' in the string will return -1
+idx = idx + 11;
+console.log(idx); // 10
+idx * 2;
+console.log(idx); // 20
 
 // 2 - 
 
+let num = 33;
+let isEven = num % 2 === 0;
+console.log(isEven); // false
+console.log(!isEven); // true
+
 // 3 - 
+
+let str1 = 'marker';
+let num = 'whiteboard'.length - str1.length;
+console.log(num); // 4
+let str2 = 'bootcamp';
+console.log(str2[num].toUpperCase()); // ?
+let char = str2[num].toLowerCase(); // ?
+console.log(char + '!'); // ?
 
 // 4 - 
 
