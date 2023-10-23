686f8b672c0b330ea61a9e2600cdd4b1fb62d4d8
nvandy23 <nvandy2373@gmail.com>
2023-10-23 08:21:21 -0400

    commit -m 'complete Task 2 & 3

--- app.js
+++ app.js
@@ -23,8 +23,97 @@
 
 
 // Task 2 - Practice JS Basics
-// 1 - 
 
+// Note: If my prediction is right I won't leave a comment for the result. If it is wrong I will make a follow up comment on the output. 
+
+
+// this is a tricky one. I'm guess undefined prints out?
+let idx = 'abcde'.indexOf('D');
+idx = idx + 11;
+console.log(idx); 
+idx * 2;
+console.log(idx); 
+ // the index value neded up printing out and then being evaluted. Interesting! 
+
+// I predict both true and false will print out since we didn't do a if/else statement here. 
+let num = 33;
+let isEven = num % 2 === 0;
+console.log(isEven); 
+console.log(!isEven); 
+
+// this one is so long, I can't make a prediction here beforehand. I'm going to have to evalate this one line by line.
+let str1 = 'marker';
+let num = 'whiteboard'.length - str1.length;
+console.log(num); // ?
+let str2 = 'bootcamp';
+console.log(str2[num].toUpperCase()); 
+let char = str2[num].toLowerCase();
+console.log(char + '!');
+// I came up with the output of 'c!' and that is what the console also had.
+
+// I think e is going to print out
+let sentence = 'coding bootcamp is awwesome ';
+let lastChar = sentence[sentence.length - 1];
+console.log(lastChar); // ?
+console.log(sentence.indexOf(lastChar));
+//oh it gave back the value of 'e' which was indexed at 21. Interesting!
+
+
+// 'younger than 30' will print out
+let age = 28; // 
+if (age > 30) {
+  console.log('older than 30');
+} else {
+  console.log('younger than 30');
+}
+// 1 - I predict short string will print out since pizza is not longer than 10 characters
+let str ='pizza'
+if (str.length > 10) {
+    console.log('long string');
+  } else {
+    console.log('short string');
+  }
+  if (str[0] === 'p') {
+    console.log('starts with p');
+  }
+  // I predict 'divisible by 3 will print out
+  let num = 12; 
+  if (num % 3 === 0) {
+    console.log('divisible by 3');
+  } else if (num % 5 === 0) {
+    console.log('divisible by 5');
+  }
+  // I predict nothing will print out
+  let num = 8; 
+  if (num % 3 === 0) {
+    console.log('divisible by 3');
+  }
+  if (num % 5 === 0) {
+    console.log('divisible by 5');
+  }
+  // the console will print out 'starts with captial!' I believe since I capitialize the first character.
+  let str = 'Nick Vanderipe'; 
+  if (str[0] === str[0].toUpperCase()) {
+    console.log('starts with a capital!');
+  }
+  if (str[str.length - 1] === str[str.length - 1].toUpperCase()) {
+    console.log('ends with a capital!');
+  }
+
+// postive and odd  will print out to the console
+  let num = 27;
+if (num > 0) {
+  console.log('positive');
+} else if (num < 0) {
+  console.log('negative');
+} else {
+  console.log(0);
+}
+if (num % 2 === 0) {
+  console.log('even');
+} else {
+  console.log('odd');
+}
 // 2 - 
 
 // 3 - 
