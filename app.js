b2e85f7112abc3df2981f442d722ce1fafa6eea6
Branko <branko.stanisic02@gmail.com>
2023-10-26 20:52:14 -0400

    Complete HW3 tasks 1-5

--- app.js
+++ app.js
@@ -1,62 +1,61 @@
 // Task 1 - Notes for JS Evaluation Predictions
+// 5 === 5.0   it is true, doesn't matter if there is a .0 after 5
 
 // List 3 expressions that surprised you or gave unexpected results, why?
-//Equality operations: true, false !=== null etc; pretty confusing
-//i%5 meaning that 'i' is a multiple of 5, interseting that we use a % simbol
-//arrays; too hard and sometimes not able to find the right answer
+// 10 % 2 === 0 = true   
+// 21 % 2 !== 0 = true
+// 21 % 2 === 1 = true         still not sure why these are true
+// 12 % 3 === 0 = true
+// 9 % 3 === 0 = true
 
 // List 2 exprsssions you felt comfortable interpreting at a glance.
-//let i = 0; i <= 100; i++
-//finding a multiple of 3 is i%3 === 0
+//5 + 5 > 8 = true
+// 6 + 6 !== 12 = false
+
 // List 1 expression you are still 'shaky' on, why?
-//a lot of JS, almost everything; arrays, console.log, !== => etc
+// 21 % 2 !== 0 = true       why is this not false?
 
 
 
 
 // Task 2 - Practice JS Basics
 // 1 - 
-let score = 100;
-score = 25;
+// let idx = 'abcde'.indexOf('D');
+// idx = idx + 11;
+// console.log(idx); // 10
+// idx * 2;
+// console.log(idx); // 10
 // 2 - 
-const months = ['Jan', 'Feb', 'March', 'April'];
-months.splice(1. 0, 'Feb');
-console.log(months)
+// let num = 33;
+// let isEven = num % 2 === 0;
+// console.log(isEven); // false
+// console.log(!isEven); // true
 // 3 - 
-function sayHelloWorld() {
-    console('HelloWorld');
-}
+// let str1 = 'marker';
+// let num = 'whiteboard'.length - str1.length;
+// console.log(num); // 4
+// let str2 = 'bootcamp';
+// console.log(str2[num].toUpperCase()); // C
+// let char = str2[num].toLowerCase(); ///
+// console.log(char + '!'); // c!
+
 // 4 - 
-const num = 30;
-if (num > 30) {
-    console.log("Guess lower!");
-if (num < 30) {
-    console.log("Guess higher!");
-else {
-    console.log("You guessed the number!")
-}}}
+// let sentence = 'welcome to bootcamp prep';
+// let lastChar = sentence[sentence.length - 1];
+// console.log(lastChar); // p
+// console.log(sentence.indexOf(lastChar)); // 18
 // 5 - 
-for (let i = 0; i <=100; i+=2) {
-console.log(i);}
+
 // 6 - 
-for (let i=0; i <50; i--){
-console.log(i);}
+
 // 7 - 
-const aboutMe = {
-    name: 'Branko';
-    age: 21;
-    hobbies: 'basketball', 'running';
-    console.log(aboutMe);
-}
+
 // 8 - 
-const isTrue = true;
-const isFalse = false;
+
 // 9 -
-const message = 'Hello' + 'World!';
+
 // 10 - 
-let count = 23;
-newCount = count + 8;
-console.log(newCount);
+
 
 //// Task 3 - Conditionals Practice 
 // 1 - 
@@ -68,24 +67,66 @@ console.log(newCount);
 // 4 - 
 
 // 5 - 
-
+// let age = 30;
+// if (age > 30) {
+//   console.log('older than 30');
+// } else {
+//   console.log('younger than 30');
+// }  // younger than 30
 // 6 - 
-
+// let str = 'cat';
+// if (str.length > 10) {
+//   console.log('long string');
+// } else {
+//   console.log('short string');
+// }
+// if (str[0] === 'p') {
+//   console.log('starts with p');
+// } // short string
 // 7 - 
-
+// let num = 20;
+// if (num % 3 === 0) {
+//   console.log('divisible by 3');
+// } else if (num % 5 === 0) {
+//   console.log('divisible by 5');
+// } // divisible by 5
 // 8 - 
-
+// let num = 12;
+// if (num % 3 === 0) {
+//   console.log('divisible by 3');
+// }
+// if (num % 5 === 0) {
+//   console.log('divisible by 5');
+// } // divisible by 3
 // 9 -
-
+// let str = 'Duke';
+// if (str[0] === str[0].toUpperCase()) {
+//   console.log('starts with a capital!');
+// }
+// if (str[str.length - 1] === str[str.length - 1].toUpperCase()) {
+//   console.log('ends with a capital!');
+// } // starts with a capital!
 // 10 - 
-
+// let num = 44;
+// if (num > 0) {
+//   console.log('positive');
+// } else if (num < 0) {
+//   console.log('negative');
+// } else {
+//   console.log(0);
+// }
+// if (num % 2 === 0) {
+//   console.log('even');
+// } else {
+//   console.log('odd');
+// } // positive; even
 //// Task 4 - Conditionals Muscle Memory
 // 1 - 
-
+// COMPLETED
 // 2 - 
-
+// COMPLETED
 // 3 - 
-
+// COMPLETED
 // 4 - 
 
 // 5 - 
@@ -103,12 +144,12 @@ console.log(newCount);
 
 //// Task 5 - Function Muscle Memory
 // 1 - 
-
+// COMPLETED
 // 2 - 
-
+// COMPLETED
 // 3 - 
-
+// COMPLETED
 // 4 - 
-
+// COMPLETED
 // 5 - 
 
